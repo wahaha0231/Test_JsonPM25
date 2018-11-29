937cd962f1ca9161a11d93d26e9085652b7f46b3
mis6 <mis6@mail.goldennet.com.tw>
2018-11-29 09:06:20 +0800

    從資料開放平台中取得PM25Json格式資料

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+using JsonSample;
+
+/*
+ https://stackoverflow.com/questions/37672423/deserialize-json-array-with-json-net-from-url 相同問題
+ [ {    }]會影響output
+*/
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            int num = 0;
+            using (var client = new HttpClient())            using (var request = new HttpRequestMessage())            {                request.Method = HttpMethod.Get;
+                //request.RequestUri = new Uri("https://opendata.epa.gov.tw/webapi/api/rest/datastore/355000000I-000259?sort=SiteName&offset=0&limit=1000");
+                request.RequestUri = new Uri("http://opendata.epa.gov.tw/webapi/Data/REWIQA/?$orderby=SiteName&$skip=0&$top=1000&format=json");
+                //request.RequestUri = new Uri("https://opendata.epa.gov.tw/ws/Data/ATM00625/?$format=json");
+                var response = await client.SendAsync(request);//請求送出去等待回應
+                var responseBody = await response.Content.ReadAsStringAsync();
+                //Rootobject tranresult = JsonConvert.DeserializeObject<Rootobject>(responseBody);
+
+                var list = JsonConvert.DeserializeObject<List<Class1>>(responseBody); //主要為List影響
+
+                
+                foreach (var item in list)
+                {
+                    num++;
+                    if (item.County == "高雄市")
+                    {
+                        Console.WriteLine($"縣市：{item.County} / 站名：{item.SiteName} / PM2.5：{item.PM25} / 說明：{item.Status}");
+                    }      
+                    
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/jsonobj.cs
@@ -0,0 +1,53 @@
+﻿
+using Newtonsoft.Json;
+
+
+namespace JsonSample
+{
+
+
+
+    public class Rootobject
+    {
+        public Class1[] Property1 { get; set; }
+    }
+
+    public class Class1
+    {
+        public string SiteName { get; set; }
+        public string County { get; set; }
+        public string AQI { get; set; }
+        public string Pollutant { get; set; }
+        public string Status { get; set; }
+        public string SO2 { get; set; }
+        public string CO { get; set; }
+        public string CO_8hr { get; set; }
+        public string O3 { get; set; }
+        public string O3_8hr { get; set; }
+        public string PM10 { get; set; }
+        //public string PM25 { get; set; }
+        public string NO2 { get; set; }
+        public string NOx { get; set; }
+        public string NO { get; set; }
+        public string WindSpeed { get; set; }
+        public string WindDirec { get; set; }
+        public string PublishTime { get; set; }
+        public string PM25_AVG { get; set; }
+        public string PM10_AVG { get; set; }
+        public string SO2_AVG { get; set; }
+        public string Longitude { get; set; }
+        public string Latitude { get; set; }
+
+        //https://dotblogs.com.tw/noncoder/2018/04/24/json-model-property-name-symbol  解決方法
+        public string _PM2x5 { get; set; }
+        [JsonProperty(PropertyName = "PM2.5")]
+        public string PM25
+        {
+            get { return _PM2x5; }
+            set { _PM2x5 = value; }
+        }
+    }
+
+
+
+}
